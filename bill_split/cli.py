a5d10d0b3d53c7b33f3e4ee64d052d54bd0e69b9
Chathan Driehuys <chathan@driehuys.com>
2019-07-15 18:42:42 -0400

    Set up basic CLI package.

---
+++ bill_split/cli.py
@@ -0,0 +1,15 @@
+#!/usr/env/python
+
+from __future__ import print_function
+
+
+def main():
+    """
+    Entry point into the program.
+    """
+    print("Hello, World!")
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+
+setup(
+    name='bill-split',
+    version='0.0.0',
+    packages=['bill_split'],
+    url='https://github.com/cdriehuys/bill-split',
+    license='MIT',
+    author='Chathan Driehuys',
+    author_email='chathan@driehuys.com',
+    description='CLI to split monthly bills.',
+    entry_points={
+        'console_scripts': ['bill-split=bill_split.cli:main']
+    }
+)
